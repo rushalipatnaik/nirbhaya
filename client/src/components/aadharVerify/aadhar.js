f76e6575b8e97f55f7b5bb39e1875889cd87b385
Sai Soumyak Nanda <saisoumyaknanda@gmail.com>
2021-10-04 17:09:11 +0000

    Aadhar Update

--- client/src/components/aadharVerify/aadhar.js
+++ client/src/components/aadharVerify/aadhar.js
@@ -3,14 +3,26 @@ import '../styles/aadhar.css'
 
 function Aadhar() {
     return (
-        <div>
-            <form>
+        <div className="aadhar">
+             <div className="aadhar-header">
+             <div className="logo">
+                 <div className="logo-name">Nirbhaya</div>
+             </div>
+             <div className="aadhar-page-title">Aadhar Authentication <i class="fas fa-fingerprint"></i></div>
+             </div>
+             <div className="aadhar-body">
+             <form>
                 <input type="file" placeholder="choose your aadhar photo"/>
                 <input type="submit" />
-                <button><a href="/userDash">User</a></button>
-                <button><a href="/herodash">Hero</a></button>
-                
             </form>
+            <button><a href="/userDash">User</a></button>
+            <button><a href="/herodash">Hero</a></button>
+            
+             </div>
+
+
+        
+
         </div>
     )
 }
