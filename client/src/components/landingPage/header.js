2f82098a347c261d7b9c2e84c5961b8926ce1991
sidmohanty11 <sidmohanty11@gmail.com>
2021-10-10 18:33:22 +0530

    google authentication

--- client/src/components/landingPage/header.js
+++ client/src/components/landingPage/header.js
@@ -1,24 +1,39 @@
 import React from 'react'
 import '../styles/header.css'
 
-function Header() {
-    return (
-        <div>
-             <div className="navbar">
-           <nav>
-              <ul>
-                  <li><a href="#home">Home</a></li>
-                  <li><a href="#about">About</a></li>
-                  <li><a href="#feature">Features</a></li>
-                  <li><a href="#footer">Contact</a></li>
-                  <li><a className="user-login" href="/userDash">User</a></li>
-                  <li><a className="hero-login" href="/heroDash">Hero</a></li>
-              </ul>
-            </nav>
-       </div>
-            
-        </div>
-    )
+function Header({ signIn }) {
+  return (
+    <div>
+      <div className="navbar">
+        <nav>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#feature">Features</a>
+            </li>
+            <li>
+              <a href="#footer">Contact</a>
+            </li>
+            <li>
+              <button onClick={signIn} className="user-login">
+                User
+              </button>
+            </li>
+            <li>
+              <button onClick={signIn} className="hero-login">
+                Hero
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
 }
 
 export default Header
--- client/src/components/landingPage/landingPage.js
+++ client/src/components/landingPage/landingPage.js
@@ -5,17 +5,32 @@ import Feature from './feature'
 import Footer from './footer'
 import Header from './header'
 import Home from './home'
+import { auth, provider } from '../../firebase'
+import { signInWithPopup } from '@firebase/auth'
 
 function LandingPage() {
-    return (
-        <div className="landingPage">
-            <Header/>
-            <Home/>
-            <About/>
-            <Feature/>
-            <Footer/>
-        </div>
-    )
+  function signIn(e) {
+    e.preventDefault()
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user
+        console.log(user)
+        // ...
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  return (
+    <div className="landingPage">
+      <Header signIn={signIn} />
+      <Home />
+      <About />
+      <Feature />
+      <Footer />
+    </div>
+  )
 }
 
 export default LandingPage
