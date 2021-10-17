d020abd202dfb228f2f43839dc0a5bec2971e843
Sai Soumyak Nanda <saisoumyaknanda@gmail.com>
2021-10-17 06:27:22 +0000

    hero-help

---
+++ client/src/components/heroDashboard/helpHero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HelpHero() {
+    return (
+        <div className="hero-help">
+            <h1 style={{'textAlign':'center'}}>Emergency Help</h1>
+        </div>
+    )
+}
+
+export default HelpHero
