48ef8b5762ac22c11aba4a65ed25f016fb51898a
Sai Soumyak Nanda <saisoumyaknanda@gmail.com>
2021-10-17 08:42:54 +0000

    btn for map

--- client/src/components/heroDashboard/helpHero.js
+++ client/src/components/heroDashboard/helpHero.js
@@ -3,11 +3,11 @@ import GoogleMapReact from 'google-map-react'
 
 function HelpHero({ data }) {
   return (
-    <div className="hero-help">
+    <div className="hero-help" id="hero-help">
       <h1 style={{ textAlign: 'center' }}>Emergency Help</h1>
 
       {data.map((d) => (
-        <div style={{ width: '100%', height: '300px', margin: '20px 0' }}>
+        <div style={{ width: '50vw', height: '400px', margin: '20px 0' }}>
           {' '}
           <GoogleMapReact
             bootstrapURLKeys={{
@@ -16,8 +16,13 @@ function HelpHero({ data }) {
             defaultCenter={d.myLoc}
             defaultZoom={18}
           >
-            <h1 style={{ color: 'red' }}>{d.name}</h1>
+          <AnyReactComponent
+              lat={myLoc.lat}
+              lng={myLoc.lng}
+              text="My Marker"
+            />
           </GoogleMapReact>
+          <button><a href={`https://www.google.com/maps/search/?api=1&query={myLoc.lat},{myLoc.lng}`}>MAP</a></button>
         </div>
       ))}
     </div>
