5e7ebc6ec034742144282efe026a683579765572
Sai Soumyak Nanda <saisoumyaknanda@gmail.com>
2021-10-17 06:41:51 +0000

    hero-help

--- client/src/components/heroDashboard/heroDash.js
+++ client/src/components/heroDashboard/heroDash.js
@@ -3,12 +3,14 @@ import Footer from '../landingPage/footer'
 import Profile from '../misc/profile'
 import HeroHeader from './heroHeader'
 import HeroHome from './heroHome'
+import HeroHelp from './helpHero'
 
 function HeroDash() {
   return (
     <div>
       <HeroHeader />
       <HeroHome />
+      <HeroHelp />
       <Profile/>
       <Footer />
     </div>
